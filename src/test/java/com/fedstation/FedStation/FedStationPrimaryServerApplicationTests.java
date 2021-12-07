66a402feffa5c75b3a22e9f35944804da871763a
TarangGarlapally <taranggarlapally@gmail.com>
2021-12-07 11:01:21 +0530

    first commit local

---
+++ src/test/java/com/fedstation/FedStation/FedStationPrimaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedstation.FedStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedStationPrimaryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
